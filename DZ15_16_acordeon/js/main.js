437207eda029b150d49610ad80f6201c44829c4c
NataliyaVergeles <79159823+NataliyaVergeles@users.noreply.github.com>
2021-04-18 22:35:02 +0300

    Add files via upload

--- DZ15_16_acordeon/js/main.js
+++ DZ15_16_acordeon/js/main.js
@@ -32,17 +32,21 @@ ulList.innerHTML += listItem;
 let listItems = doc.querySelectorAll('.list__item');
 console.log (listItems);
 for (let i = 0; i < listItems.length; i++) {
-    
+       
     listItems[i].onclick = function() {
         let content = this.querySelector('.content');
-        content.classList.toggle('content_active');
-        let allContent = doc.querySelectorAll('.content');
-        allContent.classList.remove('content_active');
-        //console.log (allContent);
-        //console.log(allContent.classList.contains('content_active')); 
-          
+        content.classList.toggle('.content');
+       
+            if (content!==listItems[i]){
+                content.classList.toggle('content_active'); 
+            }
+
+           
         
-    }
+    } 
+             
+
 }
+
 
 
